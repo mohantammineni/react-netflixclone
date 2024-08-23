f84498e68bab12df7ebfaa61ccadf145e92f9abc
Madhan Tammineni <madanmohan.t@pagesolutions.co.uk>
2024-08-23 16:50:26 +0530

    React Netflix clone inital commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,8 +16,3 @@ export const API_OPTIONS = {
 };
 
 export const CDN_IMG_URL = "https://image.tmdb.org/t/p/w500/";
-
-export const SUPPORTED_LANGUAGES = [
-  { identifier: "en", name: "English" },
-  { identifier: "spanish", name: "Spanish" },
-];
